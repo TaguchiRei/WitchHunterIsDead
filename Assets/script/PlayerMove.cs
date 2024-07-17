ecd6081dc2870302dffe27addb7a6b39ad0bad5b
TaguchiRei <ren.len2426@gmail.com>
2024-07-17 13:51:52 +0900

    作成開始 素材を作成、プレイヤーの動きを作成。

---
+++ Assets/script/PlayerMove.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField, Range(1, 15)] float _rotateSpeed = 1;
+    [SerializeField] float _moveSpeed = 1;
+    [SerializeField] Rigidbody _rig;
+    Vector3 _move = Vector3.zero;
+    float _mouseX = 0;
+    
+    void Update()
+    {
+        //マウスの動きでキャラを動かす
+        _mouseX = Input.GetAxis("Mouse X");
+        //キャラを動かす
+        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        if (vertical !=0)
+        {
+            if(vertical > 0)
+            {
+                _move += transform.forward;
+            }else
+            {
+                _move += transform.forward *-0.5f;
+            }
+        }
+        if (horizontal != 0)
+        {
+            if (horizontal < 0)
+            {
+                Debug.Log(horizontal);
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        //プレイヤーの向きを変える
+        if(Mathf.Abs(_mouseX) > 0.02)
+        {
+            transform.Rotate(0, _mouseX * _rotateSpeed, 0);
+        }
+        else if (Mathf.Abs(_mouseX) < 0.02)
+        {
+            transform.Rotate(0,_mouseX * _rotateSpeed , 0);
+        }
+        //プレイヤーを動かす
+        _move.Normalize();
+        _rig.velocity = _move * _moveSpeed;
+    }
+}
